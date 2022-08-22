8af93b5d3ef8d247742d4fb7eab12e484a0705a1
pranjal2101 <95021450+pranjal2101@users.noreply.github.com>
2022-08-22 11:20:48 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,194 @@
+<?php
+include 'inc/header.php'
+?>
+
+<div class="home-page" id="HOME">
+    <div class="row r1">
+        <div class="col-6 intro">
+        <div> <h4>THIS IS ME</h4> </div>
+        <div>
+            <span><h1>PRANJAL AGARWAL</h1></span> <br>
+        </div>
+            <!-- <h4>THIS IS ME</h4>
+            <h1>PRANJAL AGARWAL</h1> -->
+            <p>I am a positive, enthusiastic and competent Web Developer, has bild up diverse range of skills, qualities and attributes that gaurantee I will perform highly in this role.</p>
+            <!-- <button class="btn  btn-primary" type="button">DISCOVER NOW</button> -->
+            <button class="learn-more">
+                <a href="#h">
+                <span class="circle" aria-hidden="true">
+                <span class="icon arrow"></span>
+                </span>
+                <span class="button-text">Know More</span>
+                </a>
+                
+            </button>
+
+        </div>
+        <div class="col-6 intro1">
+           <img src="images/kr.jpg">
+        </div>
+    </div>
+</div>
+
+<div class="me" id="ABOUT">
+    <div class="row">
+        <div class="col-5 me-ab">
+        <img src="images/kr.jpg">
+        </div>
+
+        <div class="col-7 me-ab1" id="h">
+            <h6>ABOUT ME</h6>
+            <!-- <h1 style="font-weight:500">PERSONAL DETAILS</h1> -->
+            <p>Hi,I am Pranjal Agarwal and I am student of Computer Science and Engineering at Hindustan College Of Science and Technology. I am self taught developer. I have serious passion for modern looking websites with a little bit of animation.  I have learnt diffrent skills to inhance my capabilities for working in an organization which will provide me the opportunity to improve my skills and knowledge to growth along with the
+            organization objective.</p>
+            <!-- <button class="btn btn1 btn-primary" type="button"></button> -->
+            <div class="ani">
+                <h1>
+                    <span>I'm always be</span>
+                    <div class="message">
+                        <div class="word1">Thinking</div>
+                        <div class="word2">coding</div>
+                        <div class="word3">creating</div>
+                    </div>
+                </h1>
+            </div>
+            <button class="learn-more">
+                <a href="#h">
+                <span class="circle" aria-hidden="true">
+                <span class="icon arrow"></span>
+                </span>
+                <span class="button-text">Contact me </span>
+                </a>
+                
+            </button>
+        </div>
+    </div>
+</div>
+
+
+<div class="offer" id="SERVICE">
+    <h1> My Offered Service</h1>
+    <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, aut.</p> -->
+</div>
+
+<div class="offer-card" >
+    <div class="row">
+        <div class="col-4 oc">
+        <i class="fa-solid fa-chart-pie-simple"></i>
+        <img src="images/pie.png" class="oc-img1">
+
+            <h3>Web Design</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis maiores tempora et officiis! Optio cumque nulla aspernatur non debitis similique.</p>
+        </div>
+        <div class="col-4 oc">
+        <i class="fa-solid fa-laptop-mobile"></i>
+        <img src="images/LP.png" class="oc-img2">
+
+        <h3>Web Development</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis maiores tempora et officiis! Optio cumque nulla aspernatur non debitis similique.</p>
+        </div>
+        <div class="col-4 oc">
+        <i class="fa-solid fa-rocket-launch"></i>
+        <img src="images/rocket.png" class="oc-img3">
+        <h3>Graphic Designing</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis maiores tempora et officiis! Optio cumque nulla aspernatur non debitis similique.</p>
+        </div>
+    </div>
+</div>
+
+
+<div class="skill" id="SKILL">
+     <div class="row">
+        <div class="col-6 sk-bar">
+            <h3>My Skills and Knowledge</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, dolores neque accusamus mollitia sint, voluptate saepe quis dolorum enim expedita, corrupti aspernatur odit quas iste officia temporibus repellendus incidunt sed nisi porro. Reprehenderit a aut, itaque quidem maiores architecto nesciunt?</p>
+        </div>
+        <div class="col-6 sk-bar">
+        <div class="skl-cont">
+                <div class="sk">
+                    <h2>HTML & CSS</h2>
+                    <div class="progress-bar">
+                        <div class="html"><span>80%</span></div>
+                    </div>
+                    <h2>JavaScript</h2>
+                    <div class="progress-bar">
+                        <div class="JavaScript"><span>65%</span></div>
+                    </div>
+                    <h2>Python</h2>
+                    <div class="progress-bar">
+                        <div class="Python"><span>70%</span></div>
+                    </div>
+                    <h2>Machine Learning</h2>
+                    <div class="progress-bar">
+                        <div class="ML"><span>60%</span></div>
+                    </div>
+                </div>
+        </div>
+        </div> 
+    </div>
+</div>
+
+<section class="contact" id="CONTACT">
+    <h1 class="ct-h1">Contact Me</h1>
+    <div class="row con-row">
+        <div class="col-6 contact-info">
+            <div class="row contact-row">
+                <i class="fa-solid fa-user"></i>
+                <div class="info">
+                    <div class="head">Name</div>
+                    <div class="title">Pranjal Agarwal</div>
+                </div>
+            </div>
+            <div class="row contact-row">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <div class="info">
+                        <div class="head">Address</div>
+                        <div class="title">Near Gurudwara Maithan, Agra(282003)</div>
+                    </div>
+            </div>
+            <div class="row contact-row">
+                    <i class="fa-solid fa-envelope"></i>
+                    <div class="info">
+                        <div class="head">Email</div>
+                        <div class="title">pranjalagarwal9094@gmail.com</div>
+                    </div>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="get-in-touch">
+                <h1 class="title">Let's Get In Touch</h1>
+                <div class="contact-con">
+                    <div class="row contact-form">
+                        <div class="form-field col-lg-6">
+                            <input type="text" class="in-text" id="name">
+                            <label for="name" class="label">name</label>
+                        </div>
+                        <div class="form-field col-lg-6">
+                            <input type="email" class="in-text" id="email">
+                            <label for="name" class="label">email</label>
+                        </div>
+                        <div class="form-field col-lg-6">
+                            <input type="text" class="in-text" id="subject">
+                            <label for="name" class="label">Subject</label>
+                        </div>
+                        <div class="form-field col-lg-6">
+                            <input type="text" class="in-text" id="message">
+                            <label for="name" class="label">message</label>
+                        </div>
+                        <div class="form-field">
+                            <div class="main_div">
+                                <button type="submit">Send Message</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+</section>
+
+
+<?php
+include 'inc/footer.php'
+?>
